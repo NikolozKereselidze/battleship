ef630198065334cc37d0030fab69700a61aba545
Nika <Nika@users-MacBook-Pro.local>
2024-08-07 15:33:33 +0400

    feat: implement placeShip and isValidPlacement methods

--- src/components/Gameboard.js
+++ src/components/Gameboard.js
@@ -10,6 +10,41 @@ class Gameboard {
     this.missedAttacks = [];
     this.hitAttacks = [];
   }
+
+  placeShip(ship, x, y, direction) {
+    if (!this.isValidPlacement(ship, x, y, direction)) return;
+    if (direction === "horizontal") {
+      for (let i = 0; i < ship.length; i++) {
+        this.board[y][x + i] = ship;
+      }
+    } else {
+      for (let i = 0; i < ship.length; i++) {
+        this.board[y + i][x] = ship;
+      }
+    }
+
+    this.ships.push(ship);
+  }
+
+  isValidPlacement(ship, x, y, direction) {
+    if (x < 0 || x >= this.size || y < 0 || y >= this.size) return false;
+
+    if (direction === "horizontal") {
+      if (x > this.size - ship.length) return false;
+
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[y][x + i] !== null) return false;
+      }
+    } else if (direction === "vertical") {
+      if (y > this.size - ship.length) return false;
+
+      for (let i = 0; i < ship.length; i++) {
+        if (this.board[y + i][x] !== null) return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 module.exports = Gameboard;
